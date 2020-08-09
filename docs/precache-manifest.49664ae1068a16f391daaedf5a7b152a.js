89e17ee997d530ba565108fdd498dbd83cd6cd92
Neodevcl <neodevchile@gmail.com>
2020-08-09 03:08:30 -0400

    Docs Agregado

---
+++ docs/precache-manifest.49664ae1068a16f391daaedf5a7b152a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c38ee591ebd35bf091b89570b1fd9cd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac9607289993bdc3998c",
+    "url": "/static/css/main.32ff6147.chunk.css"
+  },
+  {
+    "revision": "a50d5cb98697f7283138",
+    "url": "/static/js/2.0ba6f1d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0ba6f1d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac9607289993bdc3998c",
+    "url": "/static/js/main.464d4ea9.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
